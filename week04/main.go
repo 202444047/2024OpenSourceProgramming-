c4a8cd4e4005824aceca08c8aa721d644e17d63f
MinBeom <bluesky460799@gmail.com>
2024-09-27 18:21:31 +0900

    v1.1

---
+++ week04/main.go
@@ -0,0 +1,16 @@
+package main
+
+import "fmt"
+
+func main() {
+	//var i int
+	//i = 55
+	//var i int =55
+	var f float32 = 1.92
+
+	i := 55
+	fmt.Println("f is", f)
+	fmt.Print("i is", i, "\n")
+	fmt.Println("i is", i)
+	fmt.Printf("i is %d\n", i)
+}
