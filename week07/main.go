cf9123bb522e9c155d273c7ca5eb62443f2cb1ec
MinBeom <bluesky460799@gmail.com>
2024-10-11 18:35:14 +0900

    v1.3 scope

--- week07/main.go
+++ week07/main.go
@@ -13,17 +13,15 @@ func main() {
 	fmt.Println("점수 입력 : ")
 	in := bufio.NewReader(os.Stdin)
 	score, err != in.ReadString('\n')
-
 	if err != nil {1
 		log.Fatal(err)
 	} 
 	score = strings.TrimSpace(score) //줄바꿈,띄어쓰기, 탭 등 제거
 	realScore, _ := strconv.ParseInt(score,16,32) //실수형 64비트 타입으로 형변환
-	if realScore >= 60 {
-		fmt.Println("A")
-		fmt.Printf("%d\n", realScore)
+	if realScore >= 90 {
+		grade :="A"
 	} else {
-		fmt.Print("BCDF")
-		fmt.Printf("%d\n", realScore)
+		grade :="BCDF"
 	}
+	fmt.Printf("%d점은 %s등급 입니다.\n", realScore,grade)
 }
