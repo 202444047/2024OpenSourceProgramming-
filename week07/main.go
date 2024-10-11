74d5112cb6af2a22740fe697496cc832c1dcfe77
MinBeom <bluesky460799@gmail.com>
2024-10-11 17:53:49 +0900

    v0.9 shadowing

--- week07/main.go
+++ week07/main.go
@@ -1,32 +1,11 @@
 package main
 
-import (
-	"bufio"
-	"fmt"
-	"log"
-	"os"
-	"strings"
-	"time"
-)
-
 func main() {
-	var now time.Time = time.Now()
-	fmt.Printf("오늘은 %d년 %d월 %d일 입니다.\n", now.Year(), int(now.Month()), now.Day())
-	fmt.Printf("지금 시각은 %d시 %d분 %d초 입니다.\n", now.Hour(), now.Minute(), now.Second())
-
-	var army string = "우리는 !가와 !민에 충성을 다하는 대한민! 육군이다."
-	armyFixed := strings.NewReplacer("!", "국")
-	fmt.Println(army)
-	fmt.Println(armyFixed)
-
-	fmt.Print("이름 입력 : ")
-	in := bufio.NewReader(os.Stdin)
-	name, err := in.ReadString('\n')
-
-	if err != nil {
-		log.Fatal(err)
-	} else {
-		fmt.Println(name)
-	}
+	//var float32 float32 = 9.1
+	//fmt.Println(float32)
+	//var number float32
+	//fmt.Println(number)
+	//var fmt float32 = 9.1
+	//fmt.Println(float32)
 
 }
