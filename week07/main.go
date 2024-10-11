42e8097a2590ae309de82e8e44d43829430b05b0
MinBeom <bluesky460799@gmail.com>
2024-10-11 18:26:02 +0900

    v1.3 ParseInt

--- week07/main.go
+++ week07/main.go
@@ -1,11 +1,29 @@
 package main
 
-func main() {
-	//var float32 float32 = 9.1
-	//fmt.Println(float32)
-	//var number float32
-	//fmt.Println(number)
-	//var fmt float32 = 9.1
-	//fmt.Println(float32)
+import (
+	"bufio"
+	"fmt"
+	"log"
+	"os"
+	"strconv"
+	"strings"
+)
 
+func main() {
+	fmt.Println("점수 입력 : ")
+	in := bufio.NewReader(os.Stdin)
+	score, err != in.ReadString('\n')
+
+	if err != nil {1
+		log.Fatal(err)
+	} 
+	score = strings.TrimSpace(score) //줄바꿈,띄어쓰기, 탭 등 제거
+	realScore, _ := strconv.ParseInt(score,16,32) //실수형 64비트 타입으로 형변환
+	if realScore >= 60 {
+		fmt.Println("A")
+		fmt.Printf("%d\n", realScore)
+	} else {
+		fmt.Print("BCDF")
+		fmt.Printf("%d\n", realScore)
+	}
 }
