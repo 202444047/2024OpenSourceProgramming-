dc6e43eea6e8c45be3600c9a22b44a37d9549dee
MinBeom <bluesky460799@gmail.com>
2024-10-25 17:01:20 +0900

    v1.7  guess game

--- week09/main.go
+++ week09/main.go
@@ -16,26 +16,29 @@ func main() {
 	answer := rand.Intn(6) + 1 // dice 1~6
 	fmt.Println(answer)
 
-	fmt.Print("점수 입력 : ")
-	in := bufio.NewReader(os.Stdin)
-	input, err := in.ReadString('\n')
-	if err != nil {
-		log.Fatal(err)
-	}
+	for guessess := 0; guessess < 3; guessess++ {
+		fmt.Print("숫자 입력 : ")
+		in := bufio.NewReader(os.Stdin)
+		input, err := in.ReadString('\n')
+		if err != nil {
+			log.Fatal(err)
+		}
 
-	input = strings.TrimSpace(input) //줄바꿈,띄어쓰기, 탭 등 제거
-	// guess, err := strconv.ParseInt(input,10,32) //정수형 32비트 타입으로 형변환, 16진수 입력
-	guess, err := strconv.Atoi(input) //정수형 32비트 타입으로 형변환, 16진수 입력
-	if err != nil {
-		log.Fatal(err)
-	}
-	fmt.Println(guess)
+		input = strings.TrimSpace(input) //줄바꿈,띄어쓰기, 탭 등 제거
+		// guess, err := strconv.ParseInt(input,10,32) //정수형 32비트 타입으로 형변환, 16진수 입력
+		guess, err := strconv.Atoi(input) //정수형 32비트 타입으로 형변환, 16진수 입력
+		if err != nil {
+			log.Fatal(err)
+		}
+		fmt.Println(guess)
 
-	if answer == guess {
-		fmt.Println("정답입니다!")
-	} else if answer > guess {
-		fmt.Println("입력하신 값은 정답보다 작은 수 입니다.")
-	} else { //answer < guess
-		fmt.Println("입력하신 값은 정답보다 큰 수 입니다.")
+		if answer == guess {
+			fmt.Println("정답입니다!")
+			break
+		} else if answer > guess {
+			fmt.Println("입력하신 값은 정답보다 작은 수 입니다.")
+		} else { //answer < guess
+			fmt.Println("입력하신 값은 정답보다 큰 수 입니다.")
+		}
 	}
 }
