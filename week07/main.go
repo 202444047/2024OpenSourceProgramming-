4b4a19a7a9e8e1e2ebe4919d023536d88cfd7eb3
MinBeom <bluesky460799@gmail.com>
2024-10-25 16:51:42 +0900

    v1.6 guess game

--- week07/main.go
+++ week07/main.go
@@ -12,16 +12,16 @@ import (
 func main() {
 	fmt.Println("점수 입력 : ")
 	in := bufio.NewReader(os.Stdin)
-	score, err != in.ReadString('\n')
-	if err != nil {1
+	score, err := in.ReadString('\n')
+	if err != nil {
 		log.Fatal(err)
-	} 
-	score = strings.TrimSpace(score) //줄바꿈,띄어쓰기, 탭 등 제거
-	realScore, _ := strconv.ParseInt(score,16,32) //정수형 32비트 타입으로 형변환, 16진수 입력
-	if realScore >= 90 {
-		grade :="A"
-	} else {
-		grade :="BCDF"
 	}
-	fmt.Printf("%d점은 %s등급 입니다.\n", realScore,grade)
+	score = strings.TrimSpace(score)                //줄바꿈,띄어쓰기, 탭 등 제거
+	realScore, _ := strconv.ParseInt(score, 16, 32) //정수형 32비트 타입으로 형변환, 16진수 입력
+	if realScore >= 90 {
+		grade := "A"
+	} else {
+		grade := "BCDF"
+	}
+	fmt.Printf("%d점은 %s등급 입니다.\n", realScore, grade)
 }
