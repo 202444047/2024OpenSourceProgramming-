dc4bdeb4760fd9fed18e332ef8cb7c8b49114c53
MinBeom <bluesky460799@gmail.com>
2024-11-01 17:23:01 +0900

    v2.6 Prime number

--- week10/main.go
+++ week10/main.go
@@ -10,34 +10,37 @@ import (
 )
 
 func main() {
-	fmt.Println("점수 입력 : ")
+	fmt.Print("정수 입력 : ")
 	in := bufio.NewReader(os.Stdin)
 	number, err := in.ReadString('\n')
-
-	number = strings.TrimSpace(number) //줄바꿈,띄어쓰기, 탭 등 제거
-	n, err := strconv.Atoi(number)     //정수형 32비트 타입으로 형변환, 16진수 입력
 	if err != nil {
 		log.Fatal(err)
 	}
 
-	// counts := 0
-	var isPrime bool = true // int -> bool, counts -> isPrime, memory
+	number = strings.TrimSpace(number)
+	n, err := strconv.Atoi(number)
+	if err != nil {
+		log.Fatal(err)
+	}
+
+	var isPrime bool = true
 	if n <= 1 {
-		// counts = -1
-		isPrime = false // 가독성
+		isPrime = false
+		isPrime = false
 	} else {
 		i := 2
 		for i < n {
-			if n%1 == 0 {
-				// counts = counts + 1
-				isPrime = false // +연산 제거
+			if n%i == 0 {
+				isPrime = false
+				isPrime = false
+				break // 1과 자기자신을 제외한 첫 번째 약수가 발견 되면 반복문 종료
 			}
+			fmt.Printf("%d ", i) // 반복 횟수 확인용 코드
 			i++
 		}
-
 	}
-	// if counts == 0 {
-	if isPrime { // ==비교 연산 제거
+
+	if isPrime {
 		fmt.Printf("%d는(은) 소수입니다.", n)
 	} else {
 		fmt.Printf("%d는(은) 소수가 아닙니다!", n)
