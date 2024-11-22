e0bea7e25d8929688be60a79c47a02ebaf45fc6a
MinBeom <bluesky460799@gmail.com>
2024-11-22 17:00:05 +0900

    v4.3 slice

--- week13/main.go
+++ week13/main.go
@@ -5,6 +5,10 @@ import (
 )
 
 func main() {
+	var emptyslice []bool
+	//emptyslice = make([]bool, 5)
+	fmt.Printf("%#v\n", emptyslice) // slice zero value (nill)
+
 	gpas := [5]float64{3.5, 4.1, 4.5, 3.9, 4.23} // array := array literal
 	gpa_slice := gpas[1:4]                       // [4.1, 4.5, 3.9]
 	//gpa_slice[1] = 2.71
